087ea4181a2120a0d020c8bebd7c2eb845233b1b
Edward Stell <edward.j.stell@gmail.com>
2021-06-29 22:54:28 +0100

    WIP implementing script to execute csv config

--- cmd/escape/README.md
+++
@@ -1,8 +0,0 @@
-# escape
-Provides a simple script to escape JSON.
-
-Simply pass your json file to the script and it'll output its content as an escaped string.
-
-```
-./escape.sh file.json
-```
---
+++ cmd/update.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+export VIMEO_ACCESS_TOKEN=e98379f7c8f78fca87bd7660a27165a1
+
+csv2json=./csv2json.jq
+vimeo=./vimeo
+template=./template
+ops_tmpl=./ops.tmpl
+
+config_pipe=/tmp/config
+
+# Check whether the access token has been set.
+if [ $VIMEO_ACCESS_TOKEN = "TODO" ]; then
+	echo "Please set your access token" >&2; exit 1
+fi
+
+# Check enough arguments passed.
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 CONFIG_CSV" >&2; exit 1
+fi
+
+# Check config file exists.
+if ! [ -f $1 ]; then
+	echo "CONFIG_CSV: Doesn't exist" >&2; exit 1
+fi
+
+# Check our csv2json converter is present.
+if ! [ -f $csv2json ]; then
+	echo "$csv2json csv converter missing" >&2; exit 1
+fi
+
+# Check the operations template exists.
+if ! [ -f $ops_tmpl ]; then
+	echo "$ops_tmpl operations template missing" >&2; exit 1
+fi
+
+# Check 'vimeo' executable present.
+if ! command -v $vimeo &> /dev/null; then
+	echo "'$vimeo' executable missing" >&2; exit 1
+fi
+
+# Check 'template' executable present.
+if ! command -v $template &> /dev/null; then
+	echo "'$template' executable missing" >&2; exit 1
+fi
+
+# Create our formatting pipe.
+if [[ ! -p $config_pipe ]]; then
+	mkfifo $config_pipe
+fi
+trap "rm -rf $config_pipe" EXIT
+
+# Convert our config csv to json for templating.
+jq -c '.[]' <(jq -R -s -f $csv2json $1) | while read i; do
+	cat $ops_tmpl | $template <(echo $i)
+done
+
+# Template config
+# Execute commands in config
